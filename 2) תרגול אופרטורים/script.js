5da11a557317d5661ccbfdc28ec75677c9d115d4
arielazulay <arielazulay9246@gmail.com>
2024-12-05 14:02:46 +0200

    תרגיל מ 8 עד 10

--- 2) תרגול אופרטורים/script.js
+++ 2) תרגול אופרטורים/script.js
@@ -1,60 +1,71 @@
 // ex-1
 
-let total= 100;
+let total = 100;
 console.log(total);
 
 // ex-2
 
-total+=5
+total += 5;
 console.log(total);
 
 // ex-3
 
-total-=5
+total -= 5;
 console.log(total);
 
 // ex-4
 
-total*=5
+total *= 5;
 console.log(total);
 
 // ex-5
 
-total /=5
+total /= 5;
 console.log(total);
 
 console.log("-------------------+5---------------------");
 
-
 // ex-6
 
-let age =27
+let age = 27;
 console.log(age);
 
 // ex-7
 
-age =age + 1
+age = age + 1;
 console.log(age);
 
-age+=1
+age += 1;
 console.log(age);
 
-age++
+age++;
 console.log(age);
 
+// ex-8
 
+age = age - 1;
+console.log(age);
 
+age -= 1;
+console.log(age);
 
+age - console.log(age + "---");
 
+// ex-9
 
+let isOk = 10 == "10";
+console.log(isOk);
 
+isOk = 10 != "10";
+console.log(isOk);
 
+// ex-10
 
+isOk = 10 === "10";
+console.log(isOk);
 
-
-
-
-
+isOk = 10 !== "10";
+console.log(isOk);
 
 
 
