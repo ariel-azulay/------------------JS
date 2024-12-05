60b6df3cc953f6edc6fe6d69a6cd5139795a84cd
arielazulay <arielazulay9246@gmail.com>
2024-12-05 07:36:33 +0200

    אופרטורים

---
+++ 2) תרגול אופרטורים/script.js
@@ -0,0 +1,61 @@
+// ex-1
+
+let total= 100;
+console.log(total);
+
+// ex-2
+
+total+=5
+console.log(total);
+
+// ex-3
+
+total-=5
+console.log(total);
+
+// ex-4
+
+total*=5
+console.log(total);
+
+// ex-5
+
+total /=5
+console.log(total);
+
+console.log("-------------------+5---------------------");
+
+
+// ex-6
+
+let age =27
+console.log(age);
+
+// ex-7
+
+age =age + 1
+console.log(age);
+
+age+=1
+console.log(age);
+
+age++
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
